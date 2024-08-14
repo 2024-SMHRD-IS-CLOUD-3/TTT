8443e2f0c5221c3c9f93c0e8eaa6c461bd34176c
smhrd <smhrd@192.168.219.62>
2024-08-14 18:06:21 +0900

    [update Scehdule]

--- TTT/src/main/java/com/smhrd/entity/Schedule.java
+++ TTT/src/main/java/com/smhrd/entity/Schedule.java
@@ -2,7 +2,6 @@ package com.smhrd.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import java.time.LocalTime;
 import java.time.OffsetTime;
 
@@ -11,12 +10,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -39,23 +36,22 @@ public class Schedule {
 	@ManyToOne
 	@JoinColumn(name = "tr_id", referencedColumnName = "tr_id", nullable = false)
 	private Trainer trainer;
-
+	
 	@Column(name="sche_content")
 	private String description;
 	
-
 	@Column(name="st_dt")
 	private LocalDate startDate;
 	
 	@Column(name="st_tm")
 	private OffsetTime startTime;
-
+	
 	@Column(name="ed_dt")
 	private LocalDate endDate;
 	
 	@Column(name="ed_tm")
 	private OffsetTime endTime;
-
+	
 	@Column(name="sche_color")	
 	private String color;
 	
@@ -65,5 +61,5 @@ public class Schedule {
 	@ManyToOne
 	@JoinColumn(name = "usr_id", referencedColumnName = "usr_id", nullable = false)
 	private User user;
-
+	
 }
